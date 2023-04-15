76a55292f5bd4f32a9ccb73f8888a620e4b84973
Leksandra87 <petroselinuma@gmail.com>
2023-04-15 11:51:55 +0300

    Lab1

--- Занятие1/Лабораторные_задания/task4_1/main.py
+++ Занятие1/Лабораторные_задания/task4_1/main.py
@@ -3,8 +3,10 @@ from itertools import count
 
 def task():
     counter = count(100, 10)
+    for _ in range(10):
+        print(next(counter))
 
-    # TODO распечатать с столбик первые 10 чисел бесконечного итератора
+    # распечатать с столбик первые 10 чисел бесконечного итератора
 
 
 if __name__ == "__main__":
